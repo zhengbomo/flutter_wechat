e55e4ae7fc29cbf10b6f9a01320099ebe173c469
zhengbomo <zhengbomo@hotmail.com>
2020-07-15 11:55:46 +0800

    pinyin for contact

--- lib/data/models/contact_info.dart
+++ lib/data/models/contact_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterwechat/data/constants/shares.dart';
+import 'package:lpinyin/lpinyin.dart';
 import 'package:tuple/tuple.dart';
 
 class ContactInfo {
@@ -9,38 +10,42 @@ class ContactInfo {
 
   static ContactInfo random() {
     const names = [
-      Tuple2('A', '阿福'),
-      Tuple2('A', '阿胶'),
-      Tuple2('A', '阿珍'),
-      Tuple2('B', '八戒'),
-      Tuple2('B', 'Bash'),
-      Tuple2('B', 'BIM'),
-      Tuple2('D', '大眼猫'),
-      Tuple2('D', '冬瓜'),
-      Tuple2('X', '小菜'),
-      Tuple2('X', '小明'),
-      Tuple2('#', '😇'),
-      Tuple2('#', '😐'),
-      Tuple2('#', '😑'),
-      Tuple2('#', '😶'),
-      Tuple2('#', '😏'),
-      Tuple2('#', '😣'),
-      Tuple2('#', '😞'),
-      Tuple2('#', '😟'),
-      Tuple2('#', '😤'),
-      Tuple2('#', '😢'),
-      Tuple2('#', '😭'),
-      Tuple2('#', '😦'),
-      Tuple2('#',
-          '😇😐😑😶😏😣😥😮😯😪😫😴😌😛😜😝😒😓😔😕😲😷😖😞😟😤😢😭😦😧😨😬😰😱😳😵😡'),
+      '阿福',
+      '阿胶',
+      '阿珍',
+      '八戒',
+      'Bash',
+      'BIM',
+      '大眼猫',
+      '冬瓜',
+      '小菜',
+      '小明',
+      '😇',
+      '😐',
+      '😑',
+      '😶',
+      '😏',
+      '😣',
+      '😞',
+      '😟',
+      '😤',
+      '😢',
+      '😭',
+      '😦',
+      '😇😐😑😶😏😣😥😮😯😪😫😴😌😛😜😝😒😓😔😕😲😷😖😞😟😤😢😭😦😧😨😬😰😱😳😵😡',
     ];
 
     var info = ContactInfo();
     info.avatar = Shares.randomColor.randomColor();
 
     final item = names.elementAt(Shares.random.nextInt(names.length));
-    info.tagIndex = item.item1;
-    info.name = item.item2;
+    String tag =
+        PinyinHelper.getFirstWordPinyin(item).substring(0, 1).toUpperCase();
+    if (!RegExp("[A-Z]").hasMatch(tag)) {
+      tag = "#";
+    }
+    info.tagIndex = tag;
+    info.name = item;
     return info;
   }
 }
