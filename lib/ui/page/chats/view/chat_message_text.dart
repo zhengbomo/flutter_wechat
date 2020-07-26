d537250d68451509d72683385113cd9b96046f09
zhengbomo <zhengbomo@hotmail.com>
2020-07-26 09:58:31 +0800

    add chat content arrow

--- lib/ui/page/chats/view/chat_message_text.dart
+++ lib/ui/page/chats/view/chat_message_text.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutterwechat/data/models/chat_message_info.dart';
 
@@ -8,13 +9,45 @@ class ChatMessageText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.fromLTRB(8, 5, 8, 5),
-        decoration: BoxDecoration(
-            color: Colors.green, borderRadius: BorderRadius.circular(3)),
-        child: Text(
-          message.content,
-          style: TextStyle(color: Colors.white, fontSize: 16),
-        ));
+    double left;
+    double right;
+    switch (this.message.userType) {
+      case MessageUserType.me:
+        right = -10.0 + 2;
+        break;
+      case MessageUserType.other:
+        left = 0.0 + 2;
+        break;
+    }
+    return Stack(
+      overflow: Overflow.visible,
+      children: <Widget>[
+        // 箭头
+        Positioned(
+          right: right,
+          left: left,
+          top: 15,
+          child: Container(
+            transform: Matrix4.identity()..rotateZ(pi / 4),
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(1.5),
+              color: Colors.green,
+            ),
+          ),
+        ),
+        // 内容
+        Container(
+          padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+          decoration: BoxDecoration(
+              color: Colors.green, borderRadius: BorderRadius.circular(3)),
+          child: Text(
+            message.content,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ],
+    );
   }
 }
